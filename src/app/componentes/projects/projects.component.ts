57137940cad4e12582dc75be98c99b38d30e8a25
Sandra <sandranfernandez@gmail.com>
2022-03-22 17:32:57 -0300

    Ensayos con binding en collapse

--- src/app/componentes/projects/projects.component.ts
+++ src/app/componentes/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ServicioService } from 'src/app/servicios/servicio.service';
+
 
 @Component({
   selector: 'app-projects',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  projectsList:any;
+  constructor(private datosPorfolio:ServicioService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+      this.projectsList = data.projects;
+    })
   }
 
 }
